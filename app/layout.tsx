632f15182048f8a0d80fd3f6f1cb3e02d4c8f401
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-06-08 12:54:27 -0500

    added page descriptions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Warmly',
+  description: 'Your go-to networking CRM.',
   generator: 'v0.dev',
 }
 
